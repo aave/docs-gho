a47fdb3fecaf627e7b6b2ffabbddb538a70fa7ec
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-01-31 18:41:15 +0000

    remove build from git and update gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/grahamnelson/Documents/gho-docs/docs-gho/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/grahamnelson/Documents/gho-docs/docs-gho/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/grahamnelson/Documents/gho-docs/docs-gho/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/grahamnelson/Documents/gho-docs/docs-gho/src/css/custom.css'),
-];
