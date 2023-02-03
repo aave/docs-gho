d86e9aa8ac1a763f9c01cba4b16f253a59e9014f
Andrew Schmidt <andrew@aave.com>
2023-02-03 01:01:12 -0600

    feat: add more to footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,167 +1,200 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'GHO Document Hub',
-  tagline: 'GHO is a decentralized, multi-collateralized stablecoin pegged to the U.S. dollar.',
-  favicon: 'img/logo.svg',
+  title: "GHO Document Hub",
+  tagline:
+    "GHO is a decentralized, multi-collateralized stablecoin pegged to the U.S. dollar.",
+  favicon: "img/logo.svg",
 
   // Set the production url of your site here
-  url: 'https://ghodocs.aave.com',
+  url: "https://ghodocs.aave.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Aave Protocol', // Usually your GitHub org/user name.
-  projectName: 'GHO', // Usually your repo name.
+  organizationName: "Aave Protocol", // Usually your GitHub org/user name.
+  projectName: "GHO", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/", // Serve the docs at the site's root
           remarkPlugins: [math],
           rehypePlugins: [katex],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'GHO Docs',
+        title: "GHO Docs",
         logo: {
-          alt: 'GHO Logo',
-          src: 'img/logo.svg',
-          srcDark: 'img/logo_dark.svg',
+          alt: "GHO Logo",
+          src: "img/logo.svg",
+          srcDark: "img/logo_dark.svg",
         },
         items: [
-        {
-          to: 'concepts/overview',
-          label: 'Concepts',
-          position: 'left',
-          className: 'V3_active',
-        },
-         {
-          to: '/developer-docs/overview',
-          label: 'Developers',
-          position: 'left',
-          className: 'V3_active',
-        },
-        {
-          to: '/gho-contracts/overview',
-          label: 'Contracts',
-          position: 'left',
-          className: 'V3_active',
-        },        {
-          to: '/GHO-FAQ',
-          label: 'FAQ',
-          position: 'left',
-          className: 'V3_active',
-        },        {
-          to: '/resources',
-          label: 'Resources',
-          position: 'left',
-          className: 'V3_active',
-        },        // {
-        //   to: '/sdk/overview',
-        //   label: 'SDK',
-        //   position: 'left',
-        //   className: 'V3_active',
-        // },
-        {
-          label: 'Techpaper',
-          to: 'https://github.com/aave/gho/blob/main/techpaper/GHO_Technical_Paper.pdf',
-          position: 'right',
-          className: 'persistent',
-        },
+          {
+            to: "concepts/overview",
+            label: "Concepts",
+            position: "left",
+            className: "V3_active",
+          },
+          {
+            to: "/developer-docs/overview",
+            label: "Developers",
+            position: "left",
+            className: "V3_active",
+          },
+          {
+            to: "/gho-contracts/overview",
+            label: "Contracts",
+            position: "left",
+            className: "V3_active",
+          },
+          {
+            to: "/GHO-FAQ",
+            label: "FAQ",
+            position: "left",
+            className: "V3_active",
+          },
+          {
+            to: "/resources",
+            label: "Resources",
+            position: "left",
+            className: "V3_active",
+          }, // {
+          //   to: '/sdk/overview',
+          //   label: 'SDK',
+          //   position: 'left',
+          //   className: 'V3_active',
+          // },
+          {
+            label: "Techpaper",
+            to: "https://github.com/aave/gho/blob/main/techpaper/GHO_Technical_Paper.pdf",
+            position: "right",
+            className: "persistent",
+          },
           // {
           //   type: 'doc',
           //   docId: 'intro',
           //   position: 'left',
           //   label: 'GHO Docs',
           // },
-        {
-          href: 'https://github.com/aave/gho',
-          label: 'GitHub',
-          position: 'right',
-          className: 'persistent',
-        },
-          {to: 'https://docs.aave.com', label: 'Aave Protocol Docs', position: 'right', className: 'persistent',},
           {
-            href: 'https://app.aave.com',
-            label: 'App',
-            position: 'right',
+            href: "https://github.com/aave/gho",
+            label: "GitHub",
+            position: "right",
+            className: "persistent",
+          },
+          {
+            to: "https://docs.aave.com",
+            label: "Aave Protocol Docs",
+            position: "right",
+            className: "persistent",
+          },
+          {
+            href: "https://app.aave.com",
+            label: "App",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Developers",
             items: [
               {
-                label: 'Home',
-                to: '/concepts/overview',
+                label: "Developers Home",
+                to: "/developer-docs/overview",
+              },
+              {
+                label: "Bug Bounty",
+                to: "https://github.com/aave/bug-bounty",
+              },
+              {
+                label: "GHO Deployed Contracts",
+                to: "/gho-contracts/overview",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Github",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/CvKUrqM',
+                label: "GHO Contracts",
+                href: "https://github.com/aave/gho",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/aaveaave',
+                label: "Aave Interface",
+                href: "https://github.com/aave/interface",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/aave',
+                label: "Aave Utilities SDK",
+                href: "https://github.com/aave/aave-utilities",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.gg/CvKUrqM",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/aaveaave",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/aave",
               },
             ],
           },
@@ -170,7 +203,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['solidity']
+        additionalLanguages: ["solidity"],
       },
     }),
 };
