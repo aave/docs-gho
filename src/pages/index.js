0ae3a28cd4d990be4bf65893f46645cfc0ce68b4
Graham Nelson <grahamnelson@live.com>
2023-02-07 13:29:45 +0100

    add metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,18 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero', styles.heroBanner)}>
+      <meta name="description" content="GHO is a decentralized multi-collateral stablecoin, transparent and native to the Aave Protocol. "/>
+        <meta property="og:url" content="https://gho.docs.com/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="GHO Document Hub | Stablecoin by Aave"/>
+        <meta property="og:description" content="GHO is a decentralized multi-collateral stablecoin, transparent and native to the Aave Protocol. "/>
+        <meta property="og:image" content="https://docs.gho.com/img/gho-docs-og.jpg"></meta>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="docs.gho.com"/>
+        <meta property="twitter:url" content="https://docs.gho.com/"/>
+        <meta name="twitter:title" content="GHO Document Hub | Stablecoin by Aave"/>
+        <meta name="twitter:description" content="GHO is a decentralized multi-collateral stablecoin, transparent and native to the Aave Protocol. "/>
+        <meta name="twitter:image" content="https://docs.gho.com/img/gho-docs-og.jpg"/>
       <div className="container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <h1 className={clsx('hero__title', styles.heroTitle)}>{siteConfig.title}</h1>
         <p className={clsx('hero__subtitle', styles.heroSubtitle)}>{siteConfig.tagline}</p>
